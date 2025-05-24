ac3ba56db3c3b17cce3a6012dc8dca02476b93ab
saadtariq-ds <tariqsaad1997@gmail.com>
2025-05-25 04:38:19 +0500

    Polymorphism

--- 01-Python-Refresher/object-oriented-programming/main.py
+++ 01-Python-Refresher/object-oriented-programming/main.py
@@ -2,22 +2,16 @@
 MAIN FILE
 """
 
+from enemy import *
 from zombie import *
 from ogre import *
 
+def battle(enemy: Enemy):
+    enemy.talk()
+    enemy.attack()
+
 zombie = Zombie(attack_damage=1, health_points=10)
-
-print(zombie.health_points)
-
-
-print(zombie.get_type_of_enemy())
-print(zombie.talk())
-print(zombie.spread_disease())
-
 ogre = Ogre(attack_damage=3, health_points=20)
 
-print(ogre.talk())
-print()
-
-print(f"{zombie.get_type_of_enemy()} has {zombie.health_points} health points and attack of {zombie.attack_damage}")
-print(f"{ogre.get_type_of_enemy()} has {ogre.health_points} health points and attack of {ogre.attack_damage}")
+battle(zombie)
+battle(ogre)
