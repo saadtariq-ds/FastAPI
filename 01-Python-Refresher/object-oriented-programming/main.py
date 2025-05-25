0ad5b5e7eb81a7eff4d96ee008ed78c7e240e287
saadtariq-ds <tariqsaad1997@gmail.com>
2025-05-25 05:13:58 +0500

    Composition and Hero Battle

--- 01-Python-Refresher/object-oriented-programming/main.py
+++ 01-Python-Refresher/object-oriented-programming/main.py
@@ -5,6 +5,7 @@ MAIN FILE
 from enemy import *
 from zombie import *
 from ogre import *
+from hero import *
 
 def battle(enemy1: Enemy, enemy2: Enemy):
     enemy1.talk()
@@ -31,8 +32,37 @@ def battle(enemy1: Enemy, enemy2: Enemy):
         print(f"{enemy2.get_type_of_enemy()} Wins")
 
 
+def hero_battle(hero: Hero, enemy: Enemy):
+
+    while hero.health_points > 0 and enemy.health_points > 0:
+        print("*************")
+
+        enemy.special_attack()
+
+        print(f"Hero: {hero.health_points} Health Points Left")
+        print(f"{enemy.get_type_of_enemy()}: {enemy.health_points} Health Points Left")
+
+        enemy.attack()
+        hero.health_points -= enemy.attack_damage
+
+        hero.attack()
+        enemy.health_points -= hero.attack_damage
+
+    print("***********")
+
+    if hero.health_points > 0:
+        print(f"Hero Wins")
+    else:
+        print(f"{enemy.get_type_of_enemy()} Wins")
+
 
 zombie = Zombie(attack_damage=1, health_points=10)
 ogre = Ogre(attack_damage=3, health_points=20)
+hero = Hero(attack_damage=1, health_points=10)
+weapon = Weapon(weapon_type="Sword", attack_increase=10)
+hero.weapon = weapon
+hero.equip_weapon()
 
-battle(enemy1=zombie, enemy2=ogre)
+# battle(enemy1=zombie, enemy2=ogre)
+# hero_battle(hero=hero, enemy=zombie)
+hero_battle(hero=hero, enemy=ogre)
