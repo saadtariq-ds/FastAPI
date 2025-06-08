8b848c44c7195ad56551024f7110b5bd01298718
saadtariq-ds <tariqsaad1997@gmail.com>
2025-06-08 07:33:29 +0500

    Add Query Parameter

--- 02-Project1-Request-Method-Logic/books.py
+++ 02-Project1-Request-Method-Logic/books.py
@@ -18,8 +18,32 @@ async def read_all_books():
     return BOOKS
 
 
+# Path Parameter
 @app.get("/books/{book_title}")
 async def read_all_books(book_title: str):
     for book in BOOKS:
         if book.get("title").casefold() == book_title.casefold():
             return book
+
+
+# Query Parameter
+@app.get("/books/")
+async def read_category_by_query(category: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get("category").casefold() == category.casefold():
+            books_to_return.append(book)
+
+    return books_to_return
+
+
+# Path and Query Parameter
+@app.get("/books/{book_author}/")
+async def read_author_category_by_query(book_author: str, category: str):
+    books_to_return = []
+    for book in BOOKS:
+        if (book.get("author").casefold() == book_author.casefold() and
+                book.get("category").casefold() == category.casefold()):
+            books_to_return.append(book)
+
+    return books_to_return
