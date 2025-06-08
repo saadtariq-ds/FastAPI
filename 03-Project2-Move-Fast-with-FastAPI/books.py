a02ed8466597c06e2b7a5c6f3adf119d293a26eb
saadtariq-ds <tariqsaad1997@gmail.com>
2025-06-09 01:04:08 +0500

    Project 2 Setup

---
+++ 03-Project2-Move-Fast-with-FastAPI/books.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI, Body
+
+app = FastAPI()
+
+
+class Book:
+    id: int
+    title: str
+    author: str
+    description: str
+    rating: str
+
+    def __init__(self, id, title, author, description, rating):
+        self.id = id
+        self.title = title
+        self.author = author
+        self.description = description
+        self.rating = rating
+
+
+BOOKS = [
+    Book(id=1, title="Computer Science Pro", author="codingwithroby", description="A very nice book!", rating=5),
+    Book(id=2, title="Be Fast with FastAPI", author="codingwithroby", description="A great book!", rating=5),
+    Book(id=3, title="Master Endpoints", author="codingwithroby", description="A awesome book!", rating=5),
+    Book(id=4, title="HP1", author="Author 1", description="Book Description", rating=2),
+    Book(id=5, title="HP2", author="Author 2", description="Book Description", rating=3),
+    Book(id=6, title="HP3", author="Author 3", description="Book Description", rating=1),
+]
+
+@app.get("/books")
+async def read_all_books():
+    return BOOKS
