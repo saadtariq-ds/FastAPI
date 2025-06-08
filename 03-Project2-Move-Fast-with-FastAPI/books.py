237d0f5b6cc120875149400409270e03252085b4
saadtariq-ds <tariqsaad1997@gmail.com>
2025-06-09 04:13:49 +0500

    Add Pydantic Validation

--- 03-Project2-Move-Fast-with-FastAPI/books.py
+++ 03-Project2-Move-Fast-with-FastAPI/books.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Body
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -18,6 +19,14 @@ class Book:
         self.rating = rating
 
 
+class BookRequest(BaseModel):
+    id: int
+    title: str
+    author: str
+    description: str
+    rating: str
+
+
 BOOKS = [
     Book(id=1, title="Computer Science Pro", author="codingwithroby", description="A very nice book!", rating=5),
     Book(id=2, title="Be Fast with FastAPI", author="codingwithroby", description="A great book!", rating=5),
@@ -33,5 +42,6 @@ async def read_all_books():
 
 
 @app.post("/create_book")
-async def create_book(book_request=Body()):
-    BOOKS.append(book_request)
+async def create_book(book_request:BookRequest):
+    new_book = Book(**book_request.model_dump())
+    BOOKS.append(new_book)
