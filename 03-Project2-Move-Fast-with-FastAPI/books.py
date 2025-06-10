e2fabf5978bcfd9ed00655cc6d80fa568304c607
saadtariq-ds <tariqsaad1997@gmail.com>
2025-06-10 12:18:07 +0500

    Add Data Validation for Path Parameters

--- 03-Project2-Move-Fast-with-FastAPI/books.py
+++ 03-Project2-Move-Fast-with-FastAPI/books.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
 from pydantic import BaseModel, Field
 from typing import Optional
 
@@ -57,7 +57,7 @@ async def read_all_books():
     return BOOKS
 
 @app.get("/books/{book_id}")
-async def read_book(book_id: int):
+async def read_book(book_id: int = Path(gt=0)):
     for book in BOOKS:
         if book.id == book_id:
             return book
@@ -95,7 +95,7 @@ async def update_book(book: BookRequest):
             BOOKS[i] = book
 
 @app.delete("/books/{book_id}")
-async def delete_book(book_id: int):
+async def delete_book(book_id: int = Path(gt=0)):
     for i in range(len(BOOKS)):
         if BOOKS[i].id == book_id:
             BOOKS.pop(i)
