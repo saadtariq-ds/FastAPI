55ad9398b78ec5b88092ceec86b79923d02f834c
saadtariq-ds <tariqsaad1997@gmail.com>
2025-06-10 11:09:28 +0500

    Add a put request

--- 03-Project2-Move-Fast-with-FastAPI/books.py
+++ 03-Project2-Move-Fast-with-FastAPI/books.py
@@ -75,6 +75,12 @@ async def create_book(book_request:BookRequest):
     BOOKS.append(find_book_id(new_book))
 
 
+@app.put("/books/update_book")
+async def update_book(book: BookRequest):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id == book.id:
+            BOOKS[i] = book
+
 
 def find_book_id(book: Book):
     book.id = 1 if len(BOOKS) == 0 else BOOKS[-1].id + 1
