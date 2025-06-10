d67fe0c13364fd7cb79e3f19d4380e034726f600
saadtariq-ds <tariqsaad1997@gmail.com>
2025-06-10 13:04:31 +0500

    Add Validation for Query Parameter

--- 03-Project2-Move-Fast-with-FastAPI/books.py
+++ 03-Project2-Move-Fast-with-FastAPI/books.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Path
+from fastapi import FastAPI, Path, Query
 from pydantic import BaseModel, Field
 from typing import Optional
 
@@ -65,7 +65,7 @@ async def read_book(book_id: int = Path(gt=0)):
 
 "Fetching Book by Rating"
 @app.get("/books/")
-async def read_book_by_rating(book_rating: int):
+async def read_book_by_rating(book_rating: int = Query(gt=0, lt=6)):
     books_to_return = []
     for book in BOOKS:
         if book.rating == book_rating:
@@ -74,7 +74,7 @@ async def read_book_by_rating(book_rating: int):
 
 "Fetching Book by Published Date"
 @app.get("/books/publish/")
-async def read_book_by_publish_date(published_date: int):
+async def read_book_by_publish_date(published_date: int = Query(gt=1999, lt=2031)):
     books_to_return = []
     for book in BOOKS:
         if book.published_date == published_date:
