c73002ee6ae0ef1dd7348aa92f08472385db9f10
saadtariq-ds <tariqsaad1997@gmail.com>
2025-06-11 16:54:35 +0500

    Fetching Records from Database

--- 04-Project3-TodoApp/main.py
+++ 04-Project3-TodoApp/main.py
@@ -1,7 +1,26 @@
-from fastapi import FastAPI
+from fastapi.params import Depends
 import models
-from database import engine
+from models import Todos
+from sqlalchemy.orm import Session
+from typing import Annotated
+from fastapi import FastAPI
+from database import engine, SESSION_LOCAL
 
 app = FastAPI()
 
 models.BASE.metadata.create_all(bind=engine)
+
+
+def get_db():
+    db = SESSION_LOCAL()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+@app.get("/")
+async def read_all(db: db_dependency):
+    return db.query(Todos).all()
