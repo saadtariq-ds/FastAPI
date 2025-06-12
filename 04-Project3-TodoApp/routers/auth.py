430aa190b84106bf3998d4df85129980f5bbbd5e
saadtariq-ds <tariqsaad1997@gmail.com>
2025-06-12 17:06:18 +0500

    Authenticating User

--- 04-Project3-TodoApp/routers/auth.py
+++ 04-Project3-TodoApp/routers/auth.py
@@ -1,10 +1,11 @@
-from fastapi import APIRouter, Path, HTTPException, Depends
+from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from database import SESSION_LOCAL
 from models import CreateUserRequest, Users
 from passlib.context import CryptContext
 from starlette import status
 from typing import Annotated
+from fastapi.security import OAuth2PasswordRequestForm
 
 
 router = APIRouter()
@@ -19,6 +20,16 @@ def get_db():
         db.close()
 
 db_dependency = Annotated[Session, Depends(get_db)]
+
+
+def authenticate_user(username: str, password: str, db):
+    user = db.query(Users).filter(Users.username == username).first()
+    if not user:
+        return False
+
+    if not bcrypt_context.verify(password, user.hashed_password):
+        return False
+    return True
 
 
 @router.post(path="/auth", status_code=status.HTTP_201_CREATED)
@@ -36,3 +47,11 @@ async def create_user(db: db_dependency, create_user_request: CreateUserRequest)
 
     db.add(create_user_model)
     db.commit()
+
+
+@router.post(path="/token")
+async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: db_dependency):
+    user = authenticate_user(form_data.username, form_data.password, db)
+    if not user:
+        return "Failed Authentication"
+    return "Successful Authentication"
