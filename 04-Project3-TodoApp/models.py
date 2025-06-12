b2049e3977e69496971e94d5d79905d424cf174b
saadtariq-ds <tariqsaad1997@gmail.com>
2025-06-12 17:46:29 +0500

    Encoding JWT Token

--- 04-Project3-TodoApp/models.py
+++ 04-Project3-TodoApp/models.py
@@ -41,3 +41,8 @@ class CreateUserRequest(BaseModel):
     last_name: str
     password: str
     role: str
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
--- 04-Project3-TodoApp/routers/auth.py
+++ 04-Project3-TodoApp/routers/auth.py
@@ -1,14 +1,19 @@
+from datetime import timedelta, datetime, timezone
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from database import SESSION_LOCAL
-from models import CreateUserRequest, Users
+from models import CreateUserRequest, Users, Token
 from passlib.context import CryptContext
 from starlette import status
 from typing import Annotated
 from fastapi.security import OAuth2PasswordRequestForm
+from jose import jwt
 
 
 router = APIRouter()
+
+SECRET_KEY = "271cd4a3ba5e0ce97d61e5cfa222c28e79f6be33693083d286bbdcfd98c7c934"
+ALGORITHM = "HS256"
 
 bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
@@ -29,7 +34,14 @@ def authenticate_user(username: str, password: str, db):
 
     if not bcrypt_context.verify(password, user.hashed_password):
         return False
-    return True
+    return user
+
+
+def create_access_token(username: str, user_id: int, expires_delta: timedelta):
+    encode = {"sub": username, "id": user_id}
+    expires = datetime.now(timezone.utc) + expires_delta
+    encode.update({"exp": expires})
+    return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
 
 
 @router.post(path="/auth", status_code=status.HTTP_201_CREATED)
@@ -49,9 +61,11 @@ async def create_user(db: db_dependency, create_user_request: CreateUserRequest)
     db.commit()
 
 
-@router.post(path="/token")
+@router.post(path="/token", response_model=Token)
 async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: db_dependency):
     user = authenticate_user(form_data.username, form_data.password, db)
     if not user:
         return "Failed Authentication"
-    return "Successful Authentication"
+
+    token = create_access_token(username=user.username, user_id=user.id, expires_delta=timedelta(minutes=20))
+    return {"access_token": token, "token_type":"bearer"}
