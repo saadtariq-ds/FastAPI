a0dd9cd78188331d5f94a7bbae8e899483c72154
saadtariq-ds <tariqsaad1997@gmail.com>
2025-06-18 14:47:59 +0500

    Adding Post Todo Request by User ID

--- 04-Project3-TodoApp/routers/todos.py
+++ 04-Project3-TodoApp/routers/todos.py
@@ -4,6 +4,7 @@ from starlette import status
 from models import Todos, TodoRequest
 from database import SESSION_LOCAL
 from typing import Annotated
+from .auth import get_current_user
 
 router = APIRouter()
 
@@ -17,6 +18,7 @@ def get_db():
 
 
 db_dependency = Annotated[Session, Depends(get_db)]
+user_dependency = Annotated[dict, Depends(get_current_user)]
 
 # Fetching all the data
 @router.get(path="/", status_code=status.HTTP_200_OK)
@@ -35,8 +37,10 @@ async def read_todo_by_id(db: db_dependency, todo_id: int = Path(gt=0)):
 
 # Creating Data
 @router.post(path="/todo", status_code=status.HTTP_201_CREATED)
-async def create_todo(db: db_dependency, todo_request: TodoRequest):
-    todo_model = Todos(**todo_request.model_dump())
+async def create_todo(user: user_dependency, db: db_dependency, todo_request: TodoRequest):
+    if user is None:
+        raise HTTPException(status_code=401, detail="Authentication Failed")
+    todo_model = Todos(**todo_request.model_dump(), owner_id=user.get("id"))
     db.add(todo_model)
     db.commit()
 
