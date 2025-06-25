14772bff3abe2484b10f52e9ef184fc5c8971c9c
saadtariq-ds <tariqsaad1997@gmail.com>
2025-06-25 17:20:53 +0500

    Connecting with Postgresql

--- 04-Project3-TodoApp/database.py
+++ 04-Project3-TodoApp/database.py
@@ -3,9 +3,14 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./todosapp.db"
+# FOR SQLITE 3
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./todosapp.db"
+# engine = create_engine(url=SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread":False})
 
-engine = create_engine(url=SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread":False})
+# FOR POSTGRES
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:test1234!@localhost/TodoApplicationDatabase"
+
+engine = create_engine(url=SQLALCHEMY_DATABASE_URL)
 
 SESSION_LOCAL = sessionmaker(autoflush=False, autocommit=False, bind=engine)
 
